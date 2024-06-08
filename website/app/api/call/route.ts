1ce4ae40f1995dd444b58ab1fb295124fadf701b
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-06-08 19:36:10 +0530

    api update

--- website/app/api/call/route.ts
+++ website/app/api/call/route.ts
@@ -8,18 +8,20 @@ let isCallEnded: boolean = false;
 let isChatMessage: boolean = false;
 let messageValue: string = "Waiting for Call";
 let chatMessageValue: string | null = null;
-
+let isAIResponse: boolean = false;
 export async function GET(request: Request) {
     console.log("API Call made to check call status")
     const currMsg=chatMessageValue;
     const currIsMsg=isChatMessage;
+    let currAI=isAIResponse;
     chatMessageValue=null;
     isChatMessage=false;
     return NextResponse.json({ 
         isCallOngoing: isCallOngoing , 
         isCallEnded : isCallEnded,
         chatMessage : currMsg,
-        isChatMessage : currIsMsg
+        isChatMessage : currIsMsg,
+        isAIResponse: currAI
     }, { status: 200 }, 
         );
 }
@@ -30,6 +32,7 @@ export async function POST(request: Request) {
     isCallOngoing = body.isCallOngoing;
     isCallEnded = body.isCallEnded;
     isChatMessage = body.isChatMessage;
+    isAIResponse = body.isAIResponse;
     if (body.isChatMessage != null){
     chatMessageValue = body.chatMessage;
     }
