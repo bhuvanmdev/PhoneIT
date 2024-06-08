601b89740fc4dda6341d0df1ef8200104af83057
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-06-08 13:52:51 +0530

    add business

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { ToastContainer} from 'react-toastify';
 
+import 'react-toastify/dist/ReactToastify.css';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <ToastContainer />
       </body>
     </html>
   );
