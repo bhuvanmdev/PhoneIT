2704f58cbc25998f4d4bcbf6667526bce8753c53
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-06-08 23:03:09 +0530

    working stage1

--- website/app/chat/page.tsx
+++ website/app/chat/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 import Chat from "@/components/Chat";
 import ResponseChat from "@/components/ResponseChat";
-import { SendIcon } from "lucide-react";
+import { ArrowRight, RefreshCcw, SendIcon } from "lucide-react";
 import { useState } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI('AIzaSyCC-9zRe_CNF_E4UKksTjXQ9bVUvtsMrVA');
+const genAI = new GoogleGenerativeAI("AIzaSyCC-9zRe_CNF_E4UKksTjXQ9bVUvtsMrVA");
 
 const page = () => {
   const [messageData, setMessageData] = useState<any>([]);
   const [inputMsg, setInputMsg] = useState("");
+  const [changed, setChanged] = useState(false);
   const handleEnter = () => {
     const restext = run(inputMsg);
     setMessageData((prevData: any) => [
@@ -20,7 +21,6 @@ const page = () => {
     setInputMsg("");
   };
   async function run(inputMsg: string) {
-
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
     const prompt = `Please act as a personal assistant who helps users with their problems. Respond to queries with clear, concise, and helpful information, providing step-by-step guidance when necessary. Be polite, professional, and empathetic in your tone. Assist with a variety of topics, including technical issues, travel planning, project advice, and more, ensuring the user's needs are met efficiently.
@@ -31,26 +31,63 @@ const page = () => {
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
+    setChanged(true);
     return text;
-    
   }
-
+  const [prevData, setPrevData] = useState([
+    [
+      { message: "Hello", sender: "user" },
+      { message: "Hi", sender: "agent" },
+      { message: "How are you?", sender: "user" },
+      { message: "I am good", sender: "agent" },
+      { message: "What are you doing?", sender: "user" },
+    ],
+    [
+      { message: "I am chatting with you", sender: "agent" },
+      { message: "What is your name?", sender: "user" },
+      { message: "I am a chatbot", sender: "agent" },
+      { message: "What is your purpose?", sender: "user" },
+      { message: "I am here to help you", sender: "agent" },
+    ],
+  ]);
   return (
     <div className="flex min-h-screen justify-between p-24">
       <div className="flex w-1/2 flex-col">
-        <div className="text-2xl">Previous conversations</div>
+        <div className="text-2xl mb-4">Previous conversations</div>
+        <div className="flex flex-col gap-4">
+          {prevData.map((data, index) => (
+            <div
+              key={index}
+              className="flex justify-between p-6 rounded-xl border-2 border-slate-800 cursor-pointer"
+              onClick={() => setMessageData(data)}
+            >
+              <div>{data[0].message}</div>
+              <ArrowRight size={16} />
+            </div>
+          ))}
+        </div>
       </div>
       <div className="flex flex-col w-1/2 relative">
         <div className="ml-4 h-full border-2 rounded-md relative">
-          <div className="p-4 text-xl absolute w-full">ChatBot</div>
+          <div className="p-4 text-xl flex gap-4 item-center absolute w-full">
+            <RefreshCcw
+              onClick={() => {
+                if (changed)
+                  setPrevData((prevData) => [...prevData, messageData]);
+                setMessageData([]);
+              }}
+              size={16}
+            />
+            <div>ChatBot</div>
+          </div>
           <div className="p-4 pr-0 justify-end flex flex-col my-12 h-xlg">
             <div className="overflow-auto pr-2">
               {messageData.length > 0 ? (
-                messageData.map((e: any) =>
+                messageData.map((e: any, i: number) =>
                   e.sender !== "user" ? (
-                    <Chat msg={e.message} />
+                    <Chat key={i} msg={e.message} />
                   ) : (
-                    <ResponseChat msg={e.message} />
+                    <ResponseChat key={i} msg={e.message} />
                   )
                 )
               ) : (
--- website/app/page.tsx
+++ website/app/page.tsx
@@ -2,21 +2,20 @@
 import Chat from "@/components/Chat";
 import ResponseChat from "@/components/ResponseChat";
 import { useState, useEffect } from "react";
-import { Mic } from 'lucide-react';
-import { doc, setDoc } from "firebase/firestore"; 
+import { Mic } from "lucide-react";
+import { doc, setDoc } from "firebase/firestore";
 import { db } from "./firebase";
 import { toast } from "react-toastify";
 
-import 'react-toastify/dist/ReactToastify.css';
-
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
   const [callStatus, setCallStatus] = useState("Waiting for call");
   const [isAIResponse, setIsAIResponse] = useState(false);
-  const [messageData, setMessageData] = useState<any>([
-  ]);
+  const [messageData, setMessageData] = useState<any>([]);
   useEffect(() => {
     const fetchCallStatus = async () => {
+      
       try {
         const response = await fetch("/api/call");
         console.log("API Call made to check call status");
@@ -28,8 +27,8 @@ export default function Home() {
         console.log("Is Message", data.isChatMessage);
         console.log("Message is", data.chatMessage);
         setIsAIResponse(data.isAIResponse);
-        if (data.isChatMessage){
-          setMessageData((prevData:any) => [...prevData, data.chatMessage]);
+        if (data.isChatMessage) {
+          setMessageData((prevData: any) => [...prevData, data.chatMessage]);
         }
         if (data.isCallEnded) {
           setCallStatus("Call Ended");
@@ -42,6 +41,7 @@ export default function Home() {
     };
 
     const intervalId = setInterval(fetchCallStatus, 2000);
+    
 
     return () => clearInterval(intervalId);
   }, []);
@@ -53,26 +53,32 @@ export default function Home() {
   return (
     <main className="flex min-h-screen justify-between p-24">
       <div className="flex w-1/2 flex-col justify-center">
-        <div className="text-4xl text-center flex flex-col items-center justify-center gap-4"
+        <div
+          className="text-4xl text-center flex flex-col items-center justify-center gap-4"
           onClick={async () => {
-            setCallStatus('Call Ended')
+            setCallStatus("Call Ended");
             const dateAndTimeNow = new Date().toLocaleString();
             //replace the / with - in the date
             const dateAndTimeNowFormatted = dateAndTimeNow.replace(/\//g, "-");
-            
-            try{await setDoc(doc(db, "calls",dateAndTimeNowFormatted ), {
-              callStatus: "Call Ended",
-              callTime: dateAndTimeNowFormatted,
-              messages: messageData,
-            });
-            toast.success("Call Ended Successfully!");
-          }catch(e){
-            console.error("Error writing document: ", e);
-            toast.error("Error writing document");
-          }
+
+            try {
+              await setDoc(doc(db, "calls", dateAndTimeNowFormatted), {
+                callStatus: "Call Ended",
+                callTime: dateAndTimeNowFormatted,
+                messages: messageData,
+              });
+              toast.success("Call Ended Successfully!");
+            } catch (e) {
+              console.error("Error writing document: ", e);
+              toast.error("Error writing document");
+            }
           }}
         >
-          {callStatus === "Call Ongoing" ? <div className="p-4 glow-icon rounded-full"><Mic className="" size={64}/></div>:null}
+          {callStatus === "Call Ongoing" ? (
+            <div className="p-4 glow-icon rounded-full">
+              <Mic className="" size={64} />
+            </div>
+          ) : null}
           {callStatus}
         </div>
       </div>
@@ -81,9 +87,19 @@ export default function Home() {
           <div className="p-4 text-xl absolute w-full">Live transcription</div>
           <div className="p-4 pr-0 justify-end flex flex-col mt-12 h-xxlg">
             <div className="overflow-auto pr-2">
-              {messageData.length>0?messageData.map((e:string, i:number) =>
-                i%2!==0 ? <Chat msg={parseAIResponse(e)} /> : <ResponseChat msg={e} />
-              ):<div className="text-center text-slate-500">Start a phone call to start receiving messages</div>}
+              {messageData.length > 0 ? (
+                messageData.map((e: string, i: number) =>
+                  i % 2 !== 0 ? (
+                    <Chat msg={parseAIResponse(e)} />
+                  ) : (
+                    <ResponseChat msg={e} />
+                  )
+                )
+              ) : (
+                <div className="text-center text-slate-500">
+                  Start a phone call to start receiving messages
+                </div>
+              )}
             </div>
           </div>
         </div>
