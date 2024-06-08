b23506ba064a33cd44e4abec13d9db97e3449684
Akash Singh <akashsingh2210670@gmail.com>
2024-06-08 13:42:24 +0530

    Created API route to handle call status
    Signed-off-by: Akash Singh <akashsingh2210670@gmail.com>

--- website/app/api/call/route.ts
+++ website/app/api/call/route.ts
@@ -1,21 +1,18 @@
+import { NextResponse } from "next/server";
+
 // app/api/call/route.ts
 
-import { NextResponse } from "next/server";
-import { wss }   from '../../server'; // Import the WebSocket server
-// import * as wss from "../../server"
+
+let flagValue: boolean = false;
+
+export async function GET(request: Request) {
+    return NextResponse.json({ flag: flagValue }, { status: 200 });
+}
+
 export async function POST(request: Request) {
-  const origin = request.headers.get('origin');
+    const origin = request.headers.get('origin');
+    const body = await request.json();
+    flagValue = body.flag;
 
-  if (origin !== 'http://localhost:3000') {
-    return NextResponse.json({ message: 'Forbidden' }, { status: 403 });
-  }
-
-  // Send a WebSocket message to all connected clients
-  wss.clients.forEach((client: any)  => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify({ message: 'Call Ongoing' }));
-    }
-  });
-
-  return NextResponse.json({ message: 'Call Ongoing' }, { status: 200 });
+    return NextResponse.json({ message: 'Call Ongoing' }, { status: 200 });
 }
--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,20 +1,28 @@
-'use client';
+'use client'
 import { useState, useEffect } from 'react';
 
 export default function Home() {
   const [callStatus, setCallStatus] = useState('Waiting for call');
 
   useEffect(() => {
-    const socket = new WebSocket('ws://localhost:3000');
-
-    socket.addEventListener('message', (event) => {
-      const data = JSON.parse(event.data);
-      setCallStatus(data.message);
-    });
-
-    return () => {
-      socket.close();
+    const fetchCallStatus = async () => {
+      try {
+        const response = await fetch('/api/call');
+        console.log("API Call made to check call status")
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log("API Call response", data.flag)
+        setCallStatus(data.flag ? 'Call Ongoing' : 'Waiting for call');
+      } catch (error) {
+        console.error('Error fetching call status:', error);
+      }
     };
+
+    const intervalId = setInterval(fetchCallStatus, 1000);
+
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
